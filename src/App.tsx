627b3e6489e0aeb220e9a3db3e52c8abce1e6ac2
Tarang Chikhalia <tarang.chikhalia@gmail.com>
2025-12-11 22:09:31 +0530

    Add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { Button } from '@/components/ui/button'
+
 
 function App() {
 
   return (
     <>
       <h1>QuizMe</h1>
+      <Button>Click Me!</Button>
     </>
   )
 }
